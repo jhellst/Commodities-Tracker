e954a3bd9eaa88dcee82c50ae296eeea6f737b42
Joshua <jhellst@gmail.com>
2024-09-10 20:58:09 -0700

    Modified seed file to correctly process extracted data.

--- seed.py
+++ seed.py
@@ -1,10 +1,10 @@
 from app import db
 from models import User, Commodity, CommodityHistoricalData, CommoditiesFollowedByUser
-from flask_bcrypt import Bcrypt
+# from flask_bcrypt import Bcrypt
 from helpers import get_commodities_list, update_historical_prices
 
 
-bcrypt = Bcrypt()
+# bcrypt = Bcrypt()
 
 db.drop_all()
 db.create_all()
@@ -18,13 +18,29 @@ print("commodities_list: ", commodities_list)
 # {'symbol': 'ESUSD', 'name': 'E-Mini S&P 500', 'currency': 'USD', 'stockExchange': 'CME', 'exchangeShortName': 'COMMODITY'}
 
 for commodity in commodities_list:
-    current_commodity = Commodity(ticker_symbol=commodity.symbol, name=commodity.name, currency=commodity.currency, stock_exchange_symbol=commodity.stockExchange, stock_exchange_name=commodity.exchangeShortName)
-#   - Then, save the data for each commodity into the db.
+    print("commodity!", commodity, commodity.get('symbol'), commodity.get('name'), commodity.get('currency'), commodity.get('stockExchange'), commodity.get('exchangeShortName'))
+
+    # TODO: Fix this functionality for seeding db.
+
+    print("1@")
+    print("commodity.get('symbol')", commodity.get('symbol'), type(commodity.get('symbol')))
+    print("commodity.get('name')", commodity.get('name'), type(commodity.get('name')))
+    print("commodity.get('currency')", commodity.get('currency'), type(commodity.get('currency')))
+    print("commodity.get('stockExchange')", commodity.get('stockExchange'), type(commodity.get('stockExchange')))
+    print("commodity.get('exchangeShortName')", commodity.get('exchangeShortName'), type(commodity.get('exchangeShortName')))
+
+
+    current_commodity = Commodity(ticker_symbol=commodity.get('symbol'), name=commodity.get('name'), currency=commodity.get('currency'), stock_exchange_symbol=commodity.get('stockExchange'), stock_exchange_name=commodity.get('exchangeShortName'))
+    print("2@")
+
+    #   - Then, save the data for each commodity into the db.
     db.session.add(current_commodity)
     db.session.commit()
 
     # 2) For each commodity in the initial list, retrieve the historical data and update the db accordingly.
     current_commodity_historical_price_data = update_historical_prices(current_commodity.ticker_symbol)
+
+    print("current_commodity_historical_price_data", current_commodity_historical_price_data)
 
     for one_day_price_data in current_commodity_historical_price_data:
         cur_day_price_data = CommodityHistoricalData(ticker_symbol=current_commodity.ticker_symbol,
@@ -35,6 +51,9 @@ for commodity in commodities_list:
                                                      percentage_change=one_day_price_data.changePercent, vwap=one_day_price_data.vwap)
         db.session.add(cur_day_price_data)
         db.session.commit()
+
+db.session.commit()
+print("Seeding Complete!")
 
 
 # [{'date': '2024-09-09', 'open': 350.5, 'high': 362.5, 'low': 349.75, 'close': 359.5, 'adjClose': 359.5, 'volume': 744, 'unadjustedVolume': 744, 'change': 9, 'changePercent': 2.57, 'vwap': 355.56, 'label': 'September 09, 24', 'changeOverTime': 0.0257}, {'date': '2024-09-08', 'open': 350.5, 'high': 359, 'low': 340.75, 'close': 353.75, 'adjClose': 353.75, 'volume': 900, 'unadjustedVolume': 900, 'change': 3.25, 'changePercent': 0.92724679, 'vwap': 351.17, 'label': 'September 08, 24', 'changeOverTime': 0.0092724679}]
