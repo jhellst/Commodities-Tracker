159509ba221a74e616c13e4d575c39fbc71afaad
Joshua <jhellst@gmail.com>
2024-09-30 15:50:24 -0700

    After initial testing, added new backend API routes to retrieve info on a single commodity/custom_index, or to retrieve all commodities/custom_indices.

--- commodities_tracker_backend/app.py
+++ commodities_tracker_backend/app.py
@@ -6,17 +6,17 @@ from flask_cors import CORS, cross_origin
 
 
 import json, jsonify
-from models import db, connect_db, User, Commodity, CommodityHistoricalData, CommoditiesFollowedByUser
-from dataClasses import CommodityInfo, CommodityHistoricalInfo
+from models import db, connect_db, User, Commodity, CommodityHistoricalData, CommoditiesFollowedByUser, CustomIndex, CommoditiesInCustomIndex
+from dataClasses import CommodityInfo, CommodityHistoricalInfo, CustomIndexInfo
 from datetime import datetime, timedelta, timezone
 from helpers import get_commodities_list, update_historical_prices
 
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
-# from flask_jwt_extended import create_access_token
-# from flask_jwt_extended import create_refresh_token
-# from flask_jwt_extended import get_jwt_identity
-# from flask_jwt_extended import jwt_required
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_refresh_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 
 load_dotenv()
@@ -27,7 +27,7 @@ CURR_USER_KEY = "curr_user"
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
-# bcrypt = Bcrypt(app)
+bcrypt = Bcrypt(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
 # app.config['SQLALCHEMY_DATABASE_URI'] =\
@@ -39,21 +39,117 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
-# app.config["JWT_SECRET_KEY"] = os.environ['SECRET_KEY']
-# app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_SECRET_KEY"] = os.environ['SECRET_KEY']
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 
-# jwt = JWTManager(app)
+jwt = JWTManager(app)
 
-# @jwt.expired_token_loader
-# def my_expired_token_callback(expired_token, date):
-#     # print("Hello from jwt_expired_decorator")
-#     return redirect(url_for('logout_user'))
+@jwt.expired_token_loader
+def my_expired_token_callback(expired_token, date):
+    # print("Hello from jwt_expired_decorator")
+    return redirect(url_for('logout_user'))
 
 
 connect_db(app)
 
+##############################################################################
+# User signup/login/logout
+
+@app.route("/register", methods=["POST"])
+def register_user():
+    username = request.json["username"]
+    password = request.json["password"]
+
+    user_exists = User.query.filter_by(username=username).first() is not None
+
+    if user_exists:
+        return jsonify({"error": "User already exists"}), 409
+    hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    new_user = User(username=username, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    user_info = {"username": username, "user_id": new_user.id}
+    access_token = create_access_token(identity=json.dumps(user_info))
+    # print("access_token@@@register", access_token)
+    return jsonify(access_token=access_token)
+
+
+@app.route("/login", methods=["POST"])
+def login_user():
+    username = request.json.get("username", None)
+    password = request.json.get("password", None)
+
+    user = User.query.filter_by(username=username).one_or_none()
+
+    # Checks if user exists.
+    if user is None:
+        return jsonify({"error": "Unauthorized"}), 401
+
+    # Checks if the password is the same as hashed password
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify({"error": "Unauthorized"}), 401
+
+    # access_token = create_refresh_token(identity=username)
+    user_info = {"username": username, "user_id": user.id}
+    access_token = create_access_token(identity=json.dumps(user_info))
+    # print("access_token@@@login", access_token)
+    return jsonify(access_token=access_token)
+
+
+@app.route("/token/<username>", methods=["GET"])
+@jwt_required()
+def get_token(username):
+    user_info = User.query.filter_by(username=username).one_or_none()
+    access_token = create_access_token(identity=json.dumps(user_info))
+    # print("current_user@token", current_user)
+    return jsonify(access_token=access_token)
+
+
+# TODO: Figure out this route with jwt library.
+@app.route("/logout", methods=["POST"])
+def logout_user():
+    # print("session@@logout", session)
+    session.pop("user_id")
+    return "200"
+
+
+@app.route("/users", methods=["GET"])
+def get_all_users():
+    user_id = session["user_id"]
+
+    if not user_id:
+        return jsonify({"error": "Unauthorized"}), 401
+
+    all_users = User.query.all()
+    users = [{"user_id": user.id, "username": user.username}
+             for user in all_users]
+
+    return jsonify(users)
+
+
+@app.route("/users/<int:user_id>", methods=["GET"])
+def get_user(user_id):
+
+    if not user_id:
+        return jsonify({"error": "Unauthorized"}), 401
+
+    user = User.query.filter_by(id=user_id).one_or_none()
+    print("user!", user)
+
+    if not user:
+        return jsonify({"error": "Unauthorized"}), 401
+
+    return jsonify({
+        "username": user.username,
+        "user_id": user.id
+    })
+
+
 
 # Routes below -> just to see content of backend API calls pre-db update.
+
 
 # @app.route('/')
 # def index():
@@ -109,6 +205,36 @@ def get_historical_prices(symbol):
     historical_data = [CommodityHistoricalInfo(day.ticker_symbol, day.date, day.open_price, day.high_price, day.low_price, day.close_price, day.adj_close_price, day.volume, day.amount_change, day.percentage_change, day.vwap) for day in raw_historical_data]
     return historical_data
 
+
+@app.get("/custom_indices")
+@cross_origin()
+def get_all_custom_indices():
+    """Returns all custom indices for the specified user."""
+    all_custom_indices = db.session.query(CustomIndex.id, CustomIndex.name)
+
+    custom_indices = [CustomIndexInfo(index.id, index.name) for index in all_custom_indices]
+    return custom_indices
+
+
+@app.get("/custom_indices/<int:id>")
+@cross_origin()
+def get_custom_index(id):
+    """Returns all custom indices for the specified user."""
+
+    custom_index_raw_info = db.session.query(
+        CustomIndex.ticker_symbol, Commodity.name, Commodity.currency, Commodity.stock_exchange_symbol, Commodity.stock_exchange_name)
+
+    commodities = [CommodityInfo(commodity.ticker_symbol, commodity.name, commodity.currency, commodity.stock_exchange_symbol, commodity.stock_exchange_name) for commodity in all_commodities]
+    return commodities
+
+
+
+
+
+# TODO:
+# Add routes to create an index, and to add/remove comoddities from that index.
+# Add a route to retrieve a user's custom indices.
+# Add a route to view data for a specified list of commodities and indices, and to filter the data by date.
 
 
 # Error route.
