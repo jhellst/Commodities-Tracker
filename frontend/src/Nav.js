0a3d39711a7dac07efecf487c8582006dfb042d9
Joshua <jhellst@gmail.com>
2024-10-09 18:00:58 -0700

    Included basic NavBar, to be adjusted later to include specific routes.

--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -4,7 +4,7 @@ import { NavLink, Link, useNavigate } from "react-router-dom";
 
 /** Renders navigation bar for website.
  * Shows login/signup/homepage only if user not logged in.
- * If user is logged in, show all links to followed_leagues/homepage.
+ * If user is logged in, show all links to commodities/custom_page/homepage.
  */
 function Nav({ user, logout }) {
 
@@ -13,17 +13,15 @@ function Nav({ user, logout }) {
       {user ?
         <>
           <NavLink to="/">Homepage</NavLink>
-          <NavLink to="/leagues">All Leagues</NavLink>
-          <NavLink to="/teams">All Teams</NavLink>
-          <NavLink to={"/users/" + user.user_id + "/leagues"}>My Followed Leagues</NavLink>
-          <NavLink to={"/users/" + user.user_id + "/teams"}>My Followed Teams</NavLink>
+          <NavLink to="/commodities">All Commodities</NavLink>
+          <NavLink to={"/users/" + user.user_id + "/commodities"}>My Tracked Commodities</NavLink>
+          <NavLink to={"/users/" + user.user_id + "/custom_index"}>My Custom Indices</NavLink>
           <Link to="/" onClick={logout}>Logout</Link>
         </>
         :
         <>
           <NavLink to="/">Homepage</NavLink>
-          <NavLink to="/leagues">All Leagues</NavLink>
-          <NavLink to="/teams">All Teams</NavLink>
+          <NavLink to="/commodities">All Commodities</NavLink>
           <NavLink to="/signup" >Sign Up</NavLink>
           <NavLink to="/login" >Login</NavLink>
         </>
