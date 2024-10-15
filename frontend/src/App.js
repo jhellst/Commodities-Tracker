aed4322c54794d5e4d1bd6181bf5460b4add179e
Joshua <jhellst@gmail.com>
2024-10-14 18:09:23 -0700

    Added several new routes to retrieve data from backend API. Included all routes to retrieve user auth with tokens from backend.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,35 +31,11 @@ function App() {
   const [isLoaded, setIsLoaded] = useState(true);
 
   const [commodities, setCommodities] = useState([]);
-  const [commodityHistoricalData, setCommodityHistoricalData] = useState([]);
+  const [commodityHistoricalData, setCommodityHistoricalData] = useState([]); // TODO: Remove?
   const [followedCommodities, setFollowedCommodities] = useState([]);
   const [customIndices, setcustomIndices] = useState([]);
 
 
-
-  useEffect(() => {
-    async function setInitialCommodities() {
-      const commodities = await getCommodities();
-      setCommodities(commodities);
-    }
-    setInitialCommodities();
-  }, []);
-
-  useEffect(() => {
-    async function setInitialCommodityHistoricalData() {
-      const commodityHistoricalData = await getCommodityHistoricalData();
-      setCommodityHistoricalData(commodityHistoricalData);
-    }
-    setInitialCommodityHistoricalData();
-  }, []);
-
-  useEffect(() => {
-    async function setInitialCustomIndices() {
-      const customIndices = await getCustomIndices();
-      setcustomIndices(customIndices);
-    }
-    setInitialCustomIndices();
-  }, []);
 
   useEffect(() => {
     async function setInitialToken() {
@@ -105,6 +81,43 @@ function App() {
     fetchUserData();
   }, [token]);
 
+
+  useEffect(() => {
+    async function setInitialCommodities() {
+      const commodities = await getCommodities();
+      setCommodities(commodities);
+    }
+    setInitialCommodities();
+  }, []);
+
+  useEffect(() => {  // TODO: Remove?
+    async function setInitialCommodityHistoricalData() {
+      const commodityHistoricalData = await getCommodityHistoricalData();
+      setCommodityHistoricalData(commodityHistoricalData);
+    }
+    setInitialCommodityHistoricalData();
+  }, []);
+
+  useEffect(() => {
+    async function setInitialCustomIndices() {
+      if (user && user.user_id && user.user_id !== undefined && user.user_id !== "") {
+        const customIndices = await getCustomIndices();
+        setcustomIndices(customIndices);
+      }
+    }
+    setInitialCustomIndices();
+  }, []);
+
+
+  useEffect(() => {
+    async function setInitialFollowedCommodities() {
+      if (user && user.user_id && user.user_id !== undefined && user.user_id !== "") {
+        const followedCommodities = await getFollowedCommodities(user.user_id);
+        setFollowedCommodities(followedCommodities);
+      }
+    }
+    setInitialFollowedCommodities();
+  }, []);
 
 
 
@@ -173,9 +186,51 @@ function App() {
   }
 
 
-  console.log("commodities@App", commodities);
-  console.log("commodityHistoricalData@App", commodityHistoricalData);
+  /** Retrieves list of followed leagues by user. */
+  async function getFollowedCommodities(user_id) {
+    if (!user_id) {
+      return [];
+    }
+    const followedCommodities = await CommoditiesTrackerApi.getFollowedCommodities(user_id);
+    return followedCommodities;
+  }
 
+  /** Retrieves list of custom indices for the current user. */
+  async function getCustomIndices(user_id) {
+    if (!user_id) {
+      return [];
+    }
+    const customIndices = await CommoditiesTrackerApi.getCustomIndices(user_id);
+    return customIndices;
+  }
+
+  /** Retrieves list of followed teams by user. */
+  async function getCustomIndex(user_id, customIndexId) {
+    if (!user_id) {
+      // return [];
+      return {};
+    }
+    const customIndex = await CommoditiesTrackerApi.getCustomIndices(user_id, customIndexId);
+    return customIndex;
+  }
+
+  async function followCommodity(user_id, ticker_symbol) {
+    await CommoditiesTrackerApi.followCommodity(user_id, ticker_symbol);
+    const followedCommodities = await getFollowedLeagues(user_id);
+    setFollowedCommodities(followedCommodities);
+  }
+
+  // TODO: Create routes to 1) create a custom index, 2) add commodity to custom_index, and 3) remove commodity from custom_index.
+  //  - Need to decide if we need to delete an empty custom index, or if we can just avoid displaying those.
+
+
+  console.log("user@App", user);
+  console.log("token@App", token);
+
+  console.log("commodities@App", commodities);
+  console.log("followedCommodities@App", followedCommodities);
+
+  console.log("commodityHistoricalData@App", commodityHistoricalData);
 
 
   return (
@@ -185,10 +240,10 @@ function App() {
         // <userContext.Provider value={{ user, token }}>
         <BrowserRouter>
           {/* <Nav user={user} logout={logout} /> */}
-          <Nav />
-          {/* <RoutesList user={user} login={login} signup={signup} getTeamDetail={getTeamDetail} getLeagueTable={getLeagueTable} leagues={leagues} teams={teams} followedLeagues={followedLeagues} followedTeams={followedTeams} followedLeagueIds={followedLeagueIds} handleSubmitFollowedLeagues={handleSubmitFollowedLeagues} followedTeamIds={followedTeamIds} handleSubmitFollowedTeams={handleSubmitFollowedTeams} followLeague={followLeague} unfollowLeague={unfollowLeague} unfollowTeam={unfollowTeam} followTeam={followTeam} /> */}
+          <Nav user={user} logout={logout} />
+          <RoutesList user={user} login={login} signup={signup} commodities={commodities} getCommodities={getCommodities} getFollowedCommodities={getFollowedCommodities} getCommodityHistoricalData={getCommodityHistoricalData} followedCommodities={followedCommodities} customIndices={customIndices} getCustomIndex={getCustomIndex} getCustomIndices={getCustomIndices} followCommodity={followCommodity} />
           {/* <RoutesList user={user} login={login} signup={signup} getCommodities={getCommodities} getCommodityHistoricalData={getCommodityHistoricalData} commodities={commodities} /> */}
-          <RoutesList getCommodities={getCommodities} getCommodityHistoricalData={getCommodityHistoricalData} commodities={commodities} />
+          {/* <RoutesList getCommodities={getCommodities} getCommodityHistoricalData={getCommodityHistoricalData} commodities={commodities} /> */}
         </BrowserRouter>
         // </userContext.Provider>
         :
